5d9aa5b290ce885df0431eb5262531b93aa79a2a
cactuscactie <44120933+cactuscactie@users.noreply.github.com>
2018-10-13 19:22:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    if (message.content === 'melon') {
+        message.reply('Here, have a picture of a delicious melon! https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/watermelon.jpg?itok=6EdNOdUo');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
